1632c4f3d58d2ee57b4e7cf8e4ecb0e933ca0095
farewelleon <farewelleonmusic@gmail.com>
2022-12-07 20:17:35 +0200

    added google style for app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Search from "./Search";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather Search Engine</h1>
       <Search />
-      <p><a href="https://github.com/farewelleon/weather-app-react" target="_blank" rel="noreferrer">Open-source code</a>, by farewelleon</p>
-    </div>
+      <Weather />
+      <footer><a className="" href="https://github.com/farewelleon/weather-app-react" target="_blank" rel="noreferrer">Open-source code</a>, by farewelleon</footer>
+      </div>
+      </div>
   );
 }
