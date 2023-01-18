bd21fb634e5d8bd5b7333323fb3814d3c4148b65
farewelleon <farewelleonmusic@gmail.com>
2023-01-18 14:00:19 +0200

    deleted h1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1></h1>
       <Weather defaultCity="Lviv"/>
       <footer><a className="" href="https://github.com/farewelleon/weather-app-react" target="_blank" rel="noreferrer">Open-source code</a>, by farewelleon</footer>
       </div>
